86f079e14883118575a91022d965bab2ecea37be
Smit <aquarocker20@gmail.com>
2019-12-13 09:30:23 +0530

    Firebase test start

---
+++ src/components/config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCaQUJ_3_Cg8gaROLxHwnZrX1ulGHeQ-jU",
+  authDomain: "my-react-apps-47c96.firebaseapp.com",
+  databaseURL: "https://my-react-apps-47c96.firebaseio.com",
+  projectId: "my-react-apps-47c96",
+  storageBucket: "my-react-apps-47c96.appspot.com",
+  messagingSenderId: "744968534805",
+  appId: "1:744968534805:web:b1147b40a95bb23e84c71c",
+  measurementId: "G-4DY7BGNBTX"
+};
+
+export default config;
