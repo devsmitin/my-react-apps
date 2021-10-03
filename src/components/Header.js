70cb729c1e51a1c6180bdf28d2fa7dac26e39cb1
smit <aquarocker20@gmail.com>
2021-10-03 18:28:38 +0530

    #hotfix: header spacing

--- src/components/Header.js
+++ src/components/Header.js
@@ -22,45 +22,50 @@ class Header extends Component {
         className="navbar navbar-expand-lg navbar-light mb-2 mb-md-3 border-bottom sticky-top"
         style={{ backgroundColor: background }}
       >
-        <span className="navbar-brand p-0">
-          <img
-            src="/owl-72.png"
-            className="app-logo"
-            alt="logo"
-            width="30"
-            height="30"
-          />
-          {title}
-        </span>
-        <button
-          className={"navbar-toggler" + (this.state.isOpen ? "" : " collapsed")}
-          type="button"
-          onClick={this.toggleNav}
-        >
-          <span className="toggle-icon">Toggle Navbar</span>
-        </button>
-        <div
-          className={
-            "navbar-collapse flex-lg-fill" + (this.state.isOpen ? " show" : "")
-          }
-        >
-          <ul className="navbar-nav ms-lg-auto">
-            {links &&
-              links.map((link, index) => {
-                return (
-                  <li className="nav-item" key={index}>
-                    <NavLink
-                      className="nav-link"
-                      exact
-                      to={link.to}
-                      onClick={this.toggleNav}
-                    >
-                      {link.title}
-                    </NavLink>
-                  </li>
-                );
-              })}
-          </ul>
+        <div class="container-fluid">
+          <span className="navbar-brand p-0">
+            <img
+              src="/owl-72.png"
+              className="app-logo"
+              alt="logo"
+              width="30"
+              height="30"
+            />
+            {title}
+          </span>
+          <button
+            className={
+              "navbar-toggler" + (this.state.isOpen ? "" : " collapsed")
+            }
+            type="button"
+            onClick={this.toggleNav}
+          >
+            <span className="toggle-icon">Toggle Navbar</span>
+          </button>
+          <div
+            className={
+              "navbar-collapse flex-lg-fill" +
+              (this.state.isOpen ? " show" : "")
+            }
+          >
+            <ul className="navbar-nav ms-lg-auto">
+              {links &&
+                links.map((link, index) => {
+                  return (
+                    <li className="nav-item" key={index}>
+                      <NavLink
+                        className="nav-link"
+                        exact
+                        to={link.to}
+                        onClick={this.toggleNav}
+                      >
+                        {link.title}
+                      </NavLink>
+                    </li>
+                  );
+                })}
+            </ul>
+          </div>
         </div>
       </nav>
     );
